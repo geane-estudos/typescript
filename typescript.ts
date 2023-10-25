d69183e6ef250b151e84d45dd49347533e6d9825
Geane Moraes Ramos <geane.ramos@contavoltz.com>
2023-10-24 21:11:48 -0300

    CHORE: início dos estudos sobre typescript

---
+++ typescript.ts
@@ -0,0 +1,99 @@
+// TIPOS (TYPES)
+
+// O tipo define o que podemos fazer com um dado
+
+const frase = 'Front End'
+const total = 100
+const empresas = ['apple', 'Microsoft'];
+const body = document.body;
+const button = document.querySelector('button');
+
+console.log(frase.toLowerCase());
+console.log(total * 2)
+console.log(empresas.map((empresa) => empresa.toLowerCase()))
+
+//ts-check@ = faz o editor indicar erros de TS
+
+// TYPE ANOTATION
+
+// No momento o JavaScript não possui uma forma de indicarmos qual será o tipo de dado.
+// Não é possível prever o argumento que será passado no parâmetro de funções, para isso 
+// são necessárias as anotações de tipos.
+
+const produtos =[
+    {
+        nome: 'O Senhor dos anéis',
+        tipo: 'livro',
+    },
+    {
+        nome: 'A Guerra dos Tronos',
+        tipo: 'livro',
+    },
+    {
+        nome: 'Dark Souls',
+        tipo: 'Jogo',
+    }
+]
+
+function filtrarLivros(dados: any) {
+    return dados.filter((item: any) => item.tipo === 'livro')
+}
+console.log(filtrarLivros(produtos));
+
+
+// INSTALAÇÃO DO TYPESCRIPT
+
+// npm install -g typescript
+// testar se está instalado: tsc 
+// um arquivo em ts não consegue ser executado no browser. Para compilar em js: tsc typescript.ts
+
+function somar(a: number,b: number) {
+    return a + b
+}
+console.log(somar(5,10)); //o arquivo compilado em js remove tudo o que tem de ts e mantém só ts
+
+// para não precisar ficar compilando o tempo todo: tsc --init + npm install -g typescript
+
+// ANOTATION (ANOTAÇÃO):
+
+// COM O TRYPESCRIPT PODEMOS INDICAR QUAL SERÁ O DADO DE UMA VARIÁVEL ATRAVÉS DE ANOTAÇÕES
+
+const produto: string = "Livro";
+const preço: number = 200;
+
+const carro: {
+    marca: string;
+    portas: number;
+} = {
+    marca: "Audi",
+    portas: 4
+}
+
+console.log(carro)
+
+
+// INFERENCE (INFERÊNCIA)
+// O TS consegue inferir o tipo de dado de expressões em JSON. Sempre que ele conseguir inferir, 
+// não é necessário fazer a anotação do dado.
+
+const product= "Livro";
+const price = 200;
+
+const car: {
+    marca: string;
+    portas: number;
+} = {
+    marca: "Audi",
+    portas: 4
+}
+
+console.log(car)
+
+// FUNÇÕES
+// As anotações são necessárias quando lidamos com funções.
+
+function sum (a: number,b: number) {
+    return a + b
+}
+sum(4,10)
+console.log(sum)
